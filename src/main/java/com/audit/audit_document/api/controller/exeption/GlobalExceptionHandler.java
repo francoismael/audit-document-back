bb7e2247ec5620eaf781be349bc908e6f663bcd2
unknown <tolojanahary.francoismael@gmail.com>
2026-09-16 09:06:55 +0300

    add error exeception

---
+++ src/main/java/com/audit/audit_document/api/controller/exeption/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.audit.audit_document.api.controller.exeption;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(
+            RuntimeException exception) {
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
+    }
+}
